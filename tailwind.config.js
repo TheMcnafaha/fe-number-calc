137eb46fe8189ae31d0b53634781602b6d372424
TheMcnafaha <jafetguzmanflores@gmail.com>
2023-10-04 22:44:31 -0400

    added starter colors to tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    colors: {
+      "tp1-main-bg": " hsl(222, 26%, 31%)",
+      "tp1-alt-bg": " hsl(223, 31%, 20%)",
+      "tp1-screen-bg": " hsl(224, 36%, 15%)",
+
+      "tp1-key-bg": " hsl(225, 21%, 49%)",
+      "tp1-key-shadow": " hsl(224, 28%, 35%)",
+      "tp1-key-alt-bg": " hsl(6, 63%, 50%)",
+      "tp1-key-alt-shadow": " hsl(6, 70%, 34%)",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
