509d81dd65b80546d2c2951cac750df4a47d025a
TheMcnafaha <jafetguzmanflores@gmail.com>
2023-10-07 20:19:10 -0400

    better tailwind color names

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,17 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     colors: {
-      "tp1-main-bg": " hsl(222, 26%, 31%)",
-      "tp1-alt-bg": " hsl(223, 31%, 20%)",
-      "tp1-screen-bg": " hsl(224, 36%, 15%)",
-
-      "tp1-key-bg": " hsl(225, 21%, 49%)",
-      "tp1-key-shadow": " hsl(224, 28%, 35%)",
-      "tp1-key-alt-bg": " hsl(6, 63%, 50%)",
-      "tp1-key-alt-shadow": " hsl(6, 70%, 34%)",
-      "tp-key": " rgba(234,227,219,255)",
-      "tp-key-border": " rgba(176,164,152,255)",
+      // new colors xdd
+      "key-text": " hsl(221, 14%, 31%)",
+      "key-border": " hsl(28, 16%, 65%)",
+      key: "hsl(30, 25%, 89%) ",
+      "key-blue": "hsl(225, 21%, 49%)",
+      "key-blue-bg": " hsl(224, 28%, 35%)",
+      "key-red": " hsl(6, 63%, 50%)",
+      "key-red-bg": " hsl(6, 70%, 34%)",
+      "body-bg": " hsl(222, 26%, 31%)",
+      "display-bg": " hsl(224, 36%, 15%)",
+      "keypad-bg": " hsl(223, 31%, 20%)",
     },
     extend: {},
   },
