74cb02ec39578c874533d5cc6d9f1b19c79497c0
TheMcnafaha <jafetguzmanflores@gmail.com>
2023-10-14 17:10:53 -0400

    added scrollbar to display

--- src/components/calculator-display/calculator-display.tsx
+++ src/components/calculator-display/calculator-display.tsx
@@ -7,8 +7,8 @@ export interface CalculatorDisplayProps {
 export const CalculatorDisplay = component$<CalculatorDisplayProps>(
   ({ input }) => {
     return (
-      <section class="bg-display-bg min-h-[68px] flex justify-end px-4 text-white  text-2xl font-bold pt-4 pb-5 my-3 w-[260px] rounded-md">
-        {input}
+      <section class="bg-display-bg min-h-[68px] flex justify-end px-4 text-white  text-2xl  font-bold pt-4  my-3 w-[260px] rounded-md">
+        <div class=" overflow-auto pb-5">{input}</div>
       </section>
     );
   },
