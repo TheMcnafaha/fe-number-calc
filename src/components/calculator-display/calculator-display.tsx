ef4c9e4a8e1fef284f647360cc8a0568c0860475
TheMcnafaha <jafetguzmanflores@gmail.com>
2023-10-28 15:14:51 -0400

    fixed css font issues

--- src/components/calculator-display/calculator-display.tsx
+++ src/components/calculator-display/calculator-display.tsx
@@ -7,7 +7,7 @@ export interface CalculatorDisplayProps {
 export const CalculatorDisplay = component$<CalculatorDisplayProps>(
   ({ input }) => {
     return (
-      <section class="bg-display-bg min-h-[74px] md:min-h-[90px] flex justify-end px-4 text-display-text  text-3xl  font-bold pt-4  my-3 md:w-[400px] w-[260px] md:text-5xl md:my-5 rounded-md">
+      <section class="bg-display-bg min-h-[74px] md:min-h-[100px] flex justify-end items-center px-4 text-display-text  text-3xl  font-bold pt-4  my-3 md:w-[400px] w-[260px] md:text-5xl md:my-5 rounded-md ">
         <div class=" overflow-auto pb-5">{input}</div>
       </section>
     );
